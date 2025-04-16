029092595a20b4280f69da9fa0b08e17da625700
EnriqueRV <sergioenri2001@gmail.com>
2025-04-16 15:40:55 -0600

    feat: implementacion incial de DataTable de noticias

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
+'use client'
+
 import Header from "@/components/header";
+import { columns } from "@/components/DataTable/columns";
+import { DataTable } from "@/components/DataTable/dataTable";
+import { useNews } from "@/hooks/useNews";
 
 export default function Home() {
+  const { data: news, isLoading } = useNews();
+
+
   return (
     <main>
       <Header />
+      <DataTable columns={columns} data={news || []} />
     </main>
   );
 }
